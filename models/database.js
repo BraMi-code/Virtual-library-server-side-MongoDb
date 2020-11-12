f6793f0ea980babc75beed97095e98c33f44d82f
brami <bramit93@gmail.com>
2020-11-12 12:23:45 +0100

    models files modified

--- models/database.js
+++ models/database.js
@@ -1,12 +1,11 @@
 const mysql = require("mysql");
-const dbConfig = require("../config/config.js");
 
 // Create a connection to the database
 const connection = mysql.createConnection({
-    host: dbConfig.HOST,
-    user: dbConfig.USER,
-    password: dbConfig.PASSWORD,
-    database: dbConfig.DB
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_DB
   });
 
 // open the MySQL connection
--- models/user_model.js
+++ models/user_model.js
@@ -1,55 +1,75 @@
-const sql = require("../database.js");
-const token =  Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
-const md5 = require("md5")
+const sql = require("./database.js"),
+      bcrypt = require('bcrypt');
 
-
-const User = {};
-
-User.login = (user, result) => {
-   var password = md5(user.password);
-   console.log("md5 pass: ", password);
-   sql.query(`SELECT * FROM users WHERE username = '${user.username}' and password = '${password}'`, (err, res) => {
-      if (err) {
-         console.log("error: ", err);
-         //res.send('Incorrect Username and/or Password!');
-         result(err, null);
-         return;
-      }
-      if (res.length) {
-         console.log("found user: ", res[0]);
-         result(null, res[0]);
-         return;
-      }
-      // not found User with the id
-      result({ msg: "user not found" }, null);
-   });
-}
-
-/*
 // constructor
 const User = function(user) {
-  this.username = user.username;
   this.email = user.email;
-  this.password = user.password;
-  this.token = token;
+  this.password = bcrypt.hashSync(user.password, 10);
 };
 
-
-User.create = (newUser, result) => {
-  sql.query("INSERT INTO users SET ?", newUser, (err, res) => {
+User.login = (user, result) => {
+  sql.query(`SELECT * FROM users_vl WHERE active = 1 and email = '${user.email}'`, (err, res) => {
     if (err) {
-      console.log("zauzet username");
       console.log("error: ", err);
       result(err, null);
       return;
     }
-    console.log("created user: ", {  token: res.token, ...newUser });
-    result(null, {  token: res.token, ...newUser });
+    if (res.length) {
+      if (bcrypt.compareSync(user.password, res[0].password)) {
+        result(null, { email: res[0].email });
+        return;
+      } else {
+        result({ msg: "Autenthication failed. Invalid email or password." }, null);
+      }
+    }
+    // not found User with the id
+    result({ msg: "Authentication failed. User not found." }, null);
   });
-};
+}
 
+User.signup = (newUser, result) => {
+  sql.query("INSERT INTO users_vl SET ?", newUser, (err, res) => {
+    if (err) {
+      console.log("error: ", err);
+        // if sql return ER_DUP_ENTRY we consider this duplicate email entry
+      // since email field is PRIMARY KEY so we return message
+      if (err.code == 'ER_DUP_ENTRY') {
+        result({ msg: "This email is already registered. Use FORGOT PASSWORD to reactivate your account." }, null);
+        return;
+      }
+      result(err, null);
+      return;
+    }
+    console.log("created user: ", { ...newUser});
+    result(null, { ...newUser});
+  });
+}
 
+User.forgot = (user, result) => {
+  sql.query("UPDATE users_vl SET password = ? WHERE email = ?", [user.password, user.email], (err, res) => {
+    if (err) {
+      console.log("error: ", err);
+      result(err, null);
+      return;
+    }
+    console.log("password changed for: ", user.email);
+    result(null, { email: user.email });
+  });
+}
 
+User.confirm = (email, result) => {
+  sql.query("UPDATE users_vl SET active = 1 WHERE email = ?", [email], (err, res) => {
+    if (err) {
+      console.log("error: ", err);
+      result(err, null);
+      return;
+    }
+    console.log("activated user: ", email);
+    result(null, { email });
+  });
+}
+
+/*
 User.findById = (password, username, result) => {
   var password = JSON.stringify(md5(password));
   console.log(JSON.stringify(md5(password)));
